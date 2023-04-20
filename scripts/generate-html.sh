5b48b53bf2be2188494a7496c424073419f2798e
Forest Belton <65484+forestbelton@users.noreply.github.com>
2023-04-20 04:37:09 -0400

    Initial commit

---
+++ scripts/generate-html.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+MATHJAX_URL=${MATHJAX_URL:-https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js}
+CSS=${CSS:-static/journal.css}
+
+DATE="$DATE"
+INPUT="$1"
+OUTPUT="${INPUT%.md}.html"
+
+pandoc \
+    --standalone \
+    --metadata title="Journal Notes" \
+    --metadata date="$DATE" \
+    "--mathjax=$MATHJAX_URL" \
+	--output "$OUTPUT" \
+    "--css=$CSS" \
+    "$INPUT"
